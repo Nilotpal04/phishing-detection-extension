d2fd50b1e2e4c2126b922814510383c18c345045
Nilotpal04 <nilotpalb2004@gmail.com>
2026-03-13 10:39:51 +0530

    Implemented URL-based phishing detection rules

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-let currentUrl = window.location.href;
+let currentUrl = window.location.href.toLowerCase();
 
 let score = 0;
 
@@ -13,6 +13,32 @@ if(!currentUrl.startsWith("https"))
 if(currentUrl.length > 75) {
     score += 10;
     console.log("Warning: URL is suspiciously long");
+}
+
+if(currentUrl.includes("@")) {
+    score += 10;
+    console.log("Warning: URL contains '@' symbol");
+}
+
+let dashCount = (currentUrl.match(/-/g) || []).length;
+
+if(dashCount > 3) {
+    score += 10;
+    console.log("Warning: Too many dashes in URL");
+}
+
+let dotCount = (currentUrl.match(/\./g) || []).length;
+
+if(dotCount > 5) {
+    score += 10;
+    console.log("Warning: Too many dots in URL");
+}
+
+let digitCount = (currentUrl.match(/\d/g) || []).length;
+
+if(digitCount > 5) {
+    score += 10;
+    console.log("Warning: Too many numbers in URL")
 }
 
 console.log("Current phishing score: ", score)
